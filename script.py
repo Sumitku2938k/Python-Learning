14096fa531cba95776ee749c9242e9b336a862d3
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-08 22:30:40 +0530

    Initial commit of script.py with basic Python examples like printing, variables, and typecasting.

---
+++ script.py
@@ -0,0 +1,27 @@
+# First Python Project
+print("Hello World")
+
+# Variables
+a = 3
+print(a)
+
+b = 55
+print(b)
+
+a = 6.2 # Float
+print(a)
+
+c = True # False (Boolean variables)
+print(c)
+
+d = "Goutam" # String Variable
+print(d)
+
+e = None # Empty (Kuch Nahi)
+print(e)
+
+f = "5" # Typecasting
+print(int(f) + 1)
+
+g = 56
+print(g + 1)
