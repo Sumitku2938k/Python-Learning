2d89580e069ee1aaca367867072eeffc1de38360
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-08 22:43:35 +0530

    Learning operators in python

---
+++ operators.py
@@ -0,0 +1,40 @@
+num1 = 10
+num2 = 2
+
+# Arithmetic Operators
+print("num1 + num2 is" , num1 + num2)
+print("num1 - num2 is" , num1 - num2)
+print("num1 * num2 is" , num1 * num2)
+print("num1 / num2 is" , num1 / num2)
+
+print("num1 // num2 is" , num1 // num2) # Remove point ke bad wali value
+print("num1 % num2 is" , num1 % num2) # Return remainder after division
+print("num1 ** num2 is" , num1 ** num2) # 10^2
+
+
+#Assignment Operator
+a = 4
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 5
+print(a)
+a /= 5
+print(a)
+
+#Comparision Operator
+x = 8
+y = 3
+z = 8
+print(x > y)
+print(x < y)
+print(x == z)
+print(x != y)
+
+# Logical Operator
+print(x == z and y != z)
+print(x != z or y != z)
+print(not(True))
+print(not(False))
+
