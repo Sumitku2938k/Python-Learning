825c9e5410fb3cb5a8a132e90ccabb9250b2d5c8
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-09 20:27:45 +0530

    Add user input example for number and type casting

---
+++ userInput.py
@@ -0,0 +1,11 @@
+#name = input("Enter your name: ")
+#print(name)
+
+#age = input() # Bina kahe input mangna
+#print(age)
+
+# Write a python program to take a number from user as an input
+num = input("Enter a no: ")
+print(type(num))
+print(int(num)+6) #Type Casting
+print(type(int(num)+6))
