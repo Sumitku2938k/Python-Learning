2e3ba6fea23ba9ff5bbfcd45236e282aa854c46a
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-10 22:45:47 +0530

    Learning about strings in Python.

---
+++ newPythonFileForString.py
@@ -0,0 +1,13 @@
+goutam = 3
+Goutam = 6
+
+a1 = "Goutam" \
+     "is a bad boy"
+a2 = 'Goutam'
+a3 = '''Goutam 
+is a good boy'''
+
+print(a1, a2, a3)
+
+# a1[0] = "john" -> not possible bcz cannot change in string i.e. String is immutable
+# print(a1)
