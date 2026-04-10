2b00022f7c3a491a737dca343794cdb1902fceae
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-10 22:47:01 +0530

    Learning deep about strings in Python. This file contains various string operations and methods.

---
+++ stringDeepDive.py
@@ -0,0 +1,14 @@
+name = 'goutam kumar'
+number = "7"
+print(name)
+
+print(name[0:3]) # start from 0 and go all the way till 2 (3-1)
+print(name[1:10])
+print(name.upper())
+print(name.capitalize())
+print(name.count('m'))
+print(name.find('m'))
+print(name.find('m', 0, 3))
+print(name.isnumeric())
+print(number.isnumeric())
+
