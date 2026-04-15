6e75a43a1a2315d9d4da4e89ae9f465a44ce9d82
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-15 22:26:24 +0530

    Adding While loop

---
+++ whileLoop.py
@@ -0,0 +1,15 @@
+#i = 0
+#while i<10:
+#    print(i)
+#    i=i+1
+
+# Infinite Loop (Always True)
+#while True:
+#    print("While loop is not finishing....")
+
+while True:
+    num = int(input("Enter a number: "))
+    print(num)
+    if num == 0:
+        break # Will terminate the while loop
+j
