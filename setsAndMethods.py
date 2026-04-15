6eca27f41508097b52d3fc4676f09b000a574065
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-15 22:23:15 +0530

    Adding sets and its methods

---
+++ setsAndMethods.py
@@ -0,0 +1,11 @@
+#Sets is a collection of well-defined objects
+a1 = {3,5,23,5,5,5}
+a2 = {3,5,23,7,8,0}
+#a1.clear() -> clear the elem in the set
+#print(a1.pop())
+#a1.add(2) -> add elm at the starting
+#print(a1.union(a2)) -> a1 ke andar a2 ke elm dal do
+#print(a1.intersection(a2)) -> common between a1 & a2
+
+print(a1)
+print(a2)
