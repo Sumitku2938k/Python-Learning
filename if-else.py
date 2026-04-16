a71766761edbf87bed61ceca371ef56bf46dfd78
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-16 21:01:22 +0530

    If  else statement example added.

---
+++ if-else.py
@@ -0,0 +1,14 @@
+age = int(input("What is your age: "))
+
+if age >= 18:
+    print("Yes, you can drive")
+elif age == 1:
+    print("Bachaa hai tu abhi")
+elif age == 2:
+    print("Chirkut hai re tu abhi")
+else:
+    print("No, you can go home")
+
+print("End of Program")
+
+# Note -> In Python else is not so much required
