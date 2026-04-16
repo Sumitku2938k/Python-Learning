fc3b4804cbc95a5eeb4bfba22f2ecd3ad6667fad
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-16 20:59:05 +0530

    Adding For Loop Examples with Break and Continue Statements

---
+++ forLoop.py
@@ -0,0 +1,24 @@
+#print(1)
+#print(2)
+#print(3)
+#print(4)
+#print(5)
+
+#for i in range(10):
+#    if i == 7:
+#        break
+#    print(i+1)
+
+#a = [1, 34, 456, 34, 234]
+#for item in a:
+#    print(item)
+
+# Note: Never accept any order from set
+#s = {3, 23, 233}
+#for key in s:
+#    print(key)
+
+for i in range(5):
+    if i == 3:
+        continue
+    print(i+1)
