9c846fd94e468d8feada67c23136e8789b6b77aa
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-19 19:52:39 +0530

    Learning functions in python

---
+++ functions.py
@@ -0,0 +1,19 @@
+#def greetHello(name, ending):
+    #    print("Hello " + name)
+    #    print(ending)
+
+#print("Executing Functions")
+#greetHello("sumit", "Thank You")
+#greetHello("Goutam", "Nice")
+#print("Done")
+
+#def letterGenerator(name, date):
+#    st = f"Hi mam,\nThis is {name} and I will not come to school on {date}" # f"" -> Kind of `` for dynamic things, \n -> next line
+#    print(st)
+
+#letterGenerator("Goutam","2020-07-21")
+
+def average(a,b):
+    return (a+b)/2
+
+print(average(34,23))
